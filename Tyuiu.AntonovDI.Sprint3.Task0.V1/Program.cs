17270cfed47e8a507da2f3c95884722f4c91da69
Даниил Антонов <Tyuiu.AntonovDI@outlook.com>
2025-10-18 11:25:14 +0500

    Add project files.

---
+++ Tyuiu.AntonovDI.Sprint3.Task0.V1/Program.cs
@@ -0,0 +1,39 @@
+﻿using Tyuiu.AntonovDI.Sprint3.Task0.V1.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #3 | Выполнил: Антонов Д. И. | ИСТНб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Оператор цикла for                                                *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #1                                                              *");
+        Console.WriteLine("* Выполнил: Антонов Даниил Иванович | ИСТНб-25-1                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу используя цикл for, которая вычисляет сумму ряда     *");
+        Console.WriteLine("* по формуле, при x = 4                                                   *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+
+        int value = 4;
+        int startValue = 1;
+        int stopValue = 15;
+
+        Console.WriteLine("Переменная X - " + value);
+        Console.WriteLine("Начало шага - ", + startValue);
+        Console.WriteLine("Конец шага - " + stopValue);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Сумма ряда - " + ds.GetSumSeries(value, startValue, stopValue));
+    }
+}

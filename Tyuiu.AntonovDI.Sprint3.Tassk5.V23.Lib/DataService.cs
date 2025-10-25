85599ca800bd745036beaf814c0d5f61825701ed
Даниил Антонов <Tyuiu.AntonovDI@outlook.com>
2025-10-25 15:19:19 +0500

    Task5

---
+++ Tyuiu.AntonovDI.Sprint3.Tassk5.V23.Lib/DataService.cs
@@ -0,0 +1,22 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.AntonovDI.Sprint3.Tassk5.V23.Lib
+{
+    public class DataService : ISprint3Task5V23
+    {
+        public double GetSumSumSeries(int x, int startValue1, int startValue2, int stopValue1, int stopValue2)
+        {
+            double res = 0;
+            int perem = x;
+             int y1 = stopValue1; int y2 = stopValue2;
+            for (int x1 = startValue1 ;x1 <= y1; x1++)
+            {
+                for (int x2 = startValue2; x2 <= y2; x2++)
+                {
+                    res += ((Math.Pow(2, x2) + 2) * (Math.Cos(x2)));
+                }
+            }
+            return Math.Round(res, 3);
+        }
+    }
+}
---
+++ Tyuiu.AntonovDI.Sprint3.Tassk5.V23.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.AntonovDI.Sprint3.Tassk5.V23.Lib;
+
+namespace Tyuiu.AntonovDI.Sprint3.Tassk5.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSumSeries()
+        {
+            DataService ds = new DataService();
+            int x1 = 1;
+            int x2 = 1;
+            int y1 = 3;
+            int y2 = 10;
+            int x = 2;
+            double res = ds.GetSumSumSeries(x, x1, x2, y1, y2);
+            double wait = -3653.176;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}

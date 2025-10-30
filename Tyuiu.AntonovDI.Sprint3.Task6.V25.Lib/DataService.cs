4201b63f1179e0336c00a9f31c106d21055b8171
Даниил Антонов <Tyuiu.AntonovDI@outlook.com>
2025-10-31 01:39:33 +0500

    Task6

---
+++ Tyuiu.AntonovDI.Sprint3.Task6.V25.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.AntonovDI.Sprint3.Task6.V25.Lib
+{
+    public class DataService : ISprint3Task6V25
+    {
+        public int GetSumTheDivisors(int startValue, int stopValue)
+        {
+            int x;
+            int sum = 0;
+            for (x = startValue; x <= stopValue; x++)
+                for (int d = 1; d < 10; d++)
+                    if (x % d == 0)
+                        sum += d;
+            return sum;
+        }
+    }
+}
---
+++ Tyuiu.AntonovDI.Sprint3.Task6.V25/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.AntonovDI.Sprint3.Task6.V25.Lib;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #3 | Выполнил: Антонов Д. И. | ИСТНб-25-1";
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Создание итогового решения по спринту                             *");
+        Console.WriteLine("* Задание #6                                                              *");
+        Console.WriteLine("* Вариант #25                                                             *");
+        Console.WriteLine("* Выполнил: Антонов Даниил Иванович | ИСТНб-25-1                          *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Напишите программу, которая ищет среди целых чисел, принадлежащих       *");
+        Console.WriteLine("*  числовому отрезку [16, 24] сумму всех делителей меньше 10              *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int startValue = 16;
+        int stopValue = 24;
+        Console.WriteLine("Начальная точка - " + startValue);
+        Console.WriteLine("Конечная точка - " + stopValue);
+
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        int res = ds.GetSumTheDivisors(startValue, stopValue);
+        Console.WriteLine("Резултат выполнения программы," + ds.GetSumTheDivisors(startValue, stopValue));
+    }
+}

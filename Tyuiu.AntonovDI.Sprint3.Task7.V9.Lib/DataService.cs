f505c90a5fa239435ec1bf1d4b25e24828ab9e38
Даниил Антонов <Tyuiu.AntonovDI@outlook.com>
2025-10-31 22:23:19 +0500

    Task7

--- Tyuiu.AntonovDI.Sprint3.Task7.V9.Lib/DataService.cs
+++ Tyuiu.AntonovDI.Sprint3.Task7.V9.Lib/DataService.cs
@@ -7,16 +7,22 @@ namespace Tyuiu.AntonovDI.Sprint3.Task7.V9.Lib
     {
         public double[] GetMassFunction(int startValue, int stopValue)
         {
-            double[] valueArray;
-            int len = (stopValue - startValue) + 1;
-            valueArray = new double[len];
-            double y;
+            double[] valueArray = new double[11];
+            
             int count = 0;
-
-            for (int x = startValue; x <= startValue; x++)
+            
+            for (int x = startValue; x <= stopValue; x++)
             {
-                y = Math.Round((((2 * x - 3) / (Math.Cos(x) - 2 * x)) + 5 * x - Math.Sin(x)), 2);
-                valueArray[count] = y;
+                if ((Math.Cos(x) - 2 * x) == 0)
+                {
+                    valueArray[count] = 0;
+                }
+                else
+                {
+                    valueArray[count] = Math.Round((((2 * x - 3) / (Math.Cos(x) - 2 * x)) + 5 * x - Math.Sin(x)), 2);
+       
+                }
+                
                 count++;
             }
 
